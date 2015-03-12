2ac137585c28a2c2b2bbece218accd88133466ce
Anthonny <anthonny.querouil@gmail.com>
2015-03-12 07:38:41 +0100

    fix(theme): fix XX ago in DateTime-format

--- src/hubpress/scripts/helpers/date.js
+++ src/hubpress/scripts/helpers/date.js
@@ -4,6 +4,7 @@
 // Formats a date using moment.js. Formats published_at by default but will also take a date as a parameter
 
 var moment          = require('moment'),
+    handlebars      = require('handlebars'),
     date;
 
 date = function (context, options) {
@@ -26,11 +27,11 @@ date = function (context, options) {
         date;
 
     if (timeago) {
-        date = moment(context).fromNow();
+        date = moment(context).format('YYYY-MM-DD HH:mm:ss') + '<ago class="ago">';
     } else {
         date = moment(context).format(f);
     }
-    return date;
+    return new handlebars.SafeString(date);
 };
 
 module.exports = date;
--- src/hubpress/scripts/helpers/ghost_foot.js
+++ src/hubpress/scripts/helpers/ghost_foot.js
@@ -19,6 +19,21 @@ ghost_foot = function (options) {
         source: 'https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js',
         version: ''
     }));
+    foot.push(utils.scriptTemplate({
+        source: 'https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment-with-locales.min.js',
+        version: ''
+    }));
+    foot.push(
+      `
+      <script type="text/javascript">
+        jQuery('ago.ago').each(function(){
+          var element = jQuery(this).parent();
+          element.html( moment(element.text()).fromNow());
+        });
+      </script>
+      `
+
+    );
 
 
     var footString = _.reduce(foot, function (memo, item) { return memo + ' ' + item; }, '');
